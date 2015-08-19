3d1e77b7911df496566b46aea376616b1afa01d8
Patrick Quinn-Graham <make-contact@pftqg.com>
2015-08-19 16:07:24 +1000

    Initial commit

---
+++ bin/is-published-to-npm.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* eslint-env node */
+/* eslint vars-on-top: 0, no-process-exit: 0 */
+
+var isPublishedToNPM = require('../src/is-published-to-npm');
+
+if (process.argv.length < 4) {
+  console.log('is-published-to-npm requires two arguments:');
+  console.log('> is-published-to-npm pkgname version');
+  process.exit(2);
+}
+
+var pkg = process.argv[2];
+var version = process.argv[3];
+
+isPublishedToNPM(pkg, version, function(err) {
+  if (err) {
+    console.log(err.message);
+    process.exit(1);
+  } else {
+    console.log(pkg + ' v' + version + ' has not been published yet!');
+  }
+});
---
+++ src/is-published-to-npm.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* eslint-env node */
+/* eslint vars-on-top: 0, no-process-exit: 0 */
+
+var npm = require('npm');
+
+module.exports = function(pkg, version, callback) {
+  npm.load({}, function(err) {
+    if (err) {
+      throw err;
+    }
+    npm.commands.view([pkg, 'versions'], true, function(err, msg) {
+      if (err && err.statusCode !== 404) {
+        callback(undefined, []);
+      } else {
+        /* eslint guard-for-in: 0 */
+        var remoteVersion = Object.keys(msg)[0];
+        var remoteVersions = msg[remoteVersion].versions;
+
+        if (~remoteVersions.indexOf(version)) {
+          callback(new Error(pkg + ' v' + version + ' has already been published'));
+        } else {
+          callback(undefined, remoteVersions);
+        }
+      }
+    });
+  });
+};
